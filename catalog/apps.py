5b003685b501e648fd03ad20749809f5b9a691a0
Ruslan Valeev <postroobotbox@gmail.com>
2016-11-12 19:12:06 +0300

    rename cinemaapp, fix .gitignore

--- catalog/apps.py
+++ catalog/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class CinemaappConfig(AppConfig):
-    name = 'cinemaapp'
+    name = 'catalog'
--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the cinemaapp index.")
+    return HttpResponse("Hello, world. You're at the catalog index.")
 
 # Create your views here.

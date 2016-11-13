aa0b23800b8453f372f152e0d3f2e2f4e4a91092
Ruslan Valeev <postroobotbox@gmail.com>
2016-11-13 15:12:29 +0300

    delete cinemaapp

--- cinema/settings.py
+++ cinema/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cinemaapp',
+    'catalog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

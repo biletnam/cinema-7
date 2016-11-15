720a7f8003b832eb292173691a1701ad8fc8dddb
timsmd <timothy96@rambler.ru>
2016-11-15 18:32:12 +0300

    created "movie_info" app, added urls.py to movie_info catalog, movie_info stated in settings.py INSTALLED_APPS

---
+++ cinema/movie_info/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MovieInfoConfig(AppConfig):
+    name = 'movie_info'
--- cinema/settings.py
+++ cinema/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'cinema.catalog',
+    'cinema.movie_info'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

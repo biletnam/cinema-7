37f234d14496c15ae61f6054e140d05d89600abc
timsmd <timothy96@rambler.ru>
2016-11-16 14:00:37 +0300

    remove dummy files

--- cinema/movie_info/urls.py
+++ cinema/movie_info/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^(?P<movie_id>[0-9]+)/$', views.info, name='movie info by id'),
     url(r'^(?P<m_id>[0-9]+)/$', views.info, name='detail'),
 ]

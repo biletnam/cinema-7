9cc6eaecbf0dd505f2da3135c0c0d36ab2d7ab88
GKazhuro <gkazhuro@iMac-Georgij.local>
2016-11-28 12:21:37 +0300

    created image model and updated poster

--- cinema/catalog/models.py
+++ cinema/catalog/models.py
@@ -2,6 +2,10 @@
 
 # Create your models here.
 from django.db import models
+from django.utils.crypto import get_random_string
+from django.db.models.signals import post_delete
+from django.dispatch.dispatcher import receiver
+import os
 
 
 class Genre(models.Model):
@@ -12,12 +16,19 @@ class Genre(models.Model):
 
 
 class Movie(models.Model):
+
+    def get_image_path(instance, filename):
+        ext = filename.split('.')[-1]
+        random_name = get_random_string(length=16)
+        filename = "%s.%s" % (random_name, ext)
+        return os.path.join('images/', filename)
+
     title = models.CharField(max_length=50, null=True)
     original_title = models.CharField(max_length=50, null=True, blank=True)
     release_date = models.DateField(null=True)
     director = models.CharField(max_length=140, null=True)
     cast = models.CharField(max_length=500, null=True)
-    poster_url = models.CharField(max_length=100, null=True)
+    poster = models.ImageField(upload_to=get_image_path, blank=True, verbose_name=title)
     trailer_url = models.CharField(max_length=100, null=True)
     summary = models.CharField(max_length=1000, null=True)
     genres = models.ManyToManyField(Genre)
@@ -25,3 +36,18 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+
+    movie = models.ForeignKey(Movie)
+    image = models.ImageField(upload_to=Movie.get_image_path, blank=True, verbose_name="image")
+
+
+@receiver(post_delete, sender=Image)
+def image_delete(sender, instance, **kwargs):
+    instance.image.delete(False)
+
+@receiver(post_delete, sender=Movie)
+def poster_delete(sender, instance, **kwargs):
+    instance.poster.delete(False)

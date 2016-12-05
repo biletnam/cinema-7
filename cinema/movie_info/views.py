8a3ca6bd13d91d5c45dd8cc483c2fa032619326a
Ruslan Valeev <postroobotbox@gmail.com>
2016-12-05 13:08:07 +0300

    fix youtube url

--- cinema/movie_info/views.py
+++ cinema/movie_info/views.py
@@ -3,5 +3,6 @@ from ..catalog.models import Movie, Genre
 
 def info(request, m_id):
     cur_movie = Movie.objects.get(id = m_id)
+    cur_movie.trailer_url = cur_movie.trailer_url.split("=",1)[1]
     context = {'cur_movie': cur_movie}
     return render(request, 'movie_info/movie_info.html', context)

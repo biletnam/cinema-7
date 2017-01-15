7ae2b93e9fe6389dba2d8c7edf95f5c27a196075
Ilya <dontmarepad@gmail.com>
2017-01-15 21:15:25 +0300

    implement new view logic

--- cinema/schedule/urls.py
+++ cinema/schedule/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(\d+)-(\d+)/$', views.show_schedule),
+    url(r'^(\d+)-(\d+)/$', views.show_concrete_schedule),
     url(r'^$', views.show_schedule)
 ]

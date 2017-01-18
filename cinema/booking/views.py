344fd5c63cc9dfdeb093028cf0953b055098a120
Ilya <dontmarepad@gmail.com>
2017-01-18 23:59:27 +0300

    implement dummy template

--- cinema/booking/views.py
+++ cinema/booking/views.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("<p>test</p>")
+
 
 def show(request,id=0):
     context = {}
-    return render('booking/index.html', context)
+    return render(request,'booking/booking.html', context)

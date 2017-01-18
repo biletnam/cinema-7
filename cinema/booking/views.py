3b99185c20a7270d883761d7c639c857a50d54e5
Ilya <dontmarepad@gmail.com>
2017-01-19 00:12:20 +0300

    create seance getter

--- cinema/booking/views.py
+++ cinema/booking/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from cinema.schedule.models import Seance
 
 
 def show(request,id=0):
+    seance =  Seance.objects.get(id=id)
+    hall = seance.hall
+    row_count = hall.row_count
+    print("RESULT " + str(row_count))
+
     context = {}
     return render(request,'booking/booking.html', context)

aa2e54dc8cc6269103fc2207adc2be225cddfb72
Ilya <dontmarepad@gmail.com>
2017-01-18 22:57:54 +0300

    update view with dummy data

--- cinema/booking/views.py
+++ cinema/booking/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("<p>test</p>")
+
+def show(request,id=0):
+    context = {}
+    return render('booking/index.html', context)

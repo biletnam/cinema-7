8fecbf6533a468a776fbbece5a07e92c98e0199c
timsmd <timothy96@rambler.ru>
2017-01-23 15:12:05 +0300

    changed urls and template for selection in booking/schedule + created ajax for transmitting booking data

--- cinema/booking/urls.py
+++ cinema/booking/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(\d+)/$', views.show),
+    url(r'^(\d+)/select/$', views.show),
+    # url(r'^(\d+)/$', views.show),
 ]

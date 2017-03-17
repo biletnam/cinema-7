ce6ad9ba558080bac697cc560039e2fed9c46f91
Timofey Nazarov <timothy96@rambler.ru>
2017-03-17 19:54:41 +0300

    (50) routing now includes sign up

--- cinema/account/urls.py
+++ cinema/account/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(\d+)/login', views.auth_user),
+    url(r'^login/$', views.auth_user),
+    url(r'^signup/$', views.create_user),
 ]

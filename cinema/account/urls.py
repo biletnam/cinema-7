daa9f6e4c3ffdc33a6d5887833e761f5b74f9d44
Timofey Nazarov <timothy96@rambler.ru>
2017-03-17 21:21:45 +0300

    (45-51) added post request routing logic

--- cinema/account/urls.py
+++ cinema/account/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^login/$', views.login_view),
     url(r'^signup/$', views.signup_view),
+    url(r'^login/attempt$', views.auth_user),
+    url(r'^signup/attempt$', views.create_user),
     url(r'^(?P<id>[0-9]+)/', views.account_info),
 ]

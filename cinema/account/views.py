b7afab011d14f3a9438e1abf3de89f03bae4f42a
Timofey Nazarov <timothy96@rambler.ru>
2017-03-17 19:56:01 +0300

    (45) views create and authentificate dummy users

--- cinema/account/views.py
+++ cinema/account/views.py
@@ -1,9 +1,14 @@
-# from django.shortcuts import render
-# from cinema.account.models import UserManager, User
-# # from django.contrib.auth import authenticate as auth
-# import json
-#
-# def auth_user(request, user_json):
-#     user_info = json.loads(user_json)
-#     print(user_info)
-#     return
+from django.shortcuts import render, HttpResponse
+from cinema.account.models import UserManager, User
+from django.contrib.auth import authenticate
+import json
+
+def auth_user(request, user_json='{"email": "test@test.com", "password": "12345678"}'):
+    user_info = json.loads(user_json)
+    user = authenticate()
+    return HttpResponse('ok')
+
+def create_user(request, user_json='{"email": "test@test.com", "password": "12345678"}'):
+    user_info = json.loads(user_json)
+    user = User.objects.create_user(email=user_info["email"], password=user_info["password"])
+    return HttpResponse("user"+user.email+"created")

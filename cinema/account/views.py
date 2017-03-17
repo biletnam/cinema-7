f3344ff01d4acd83fd14fe1352e8514b7bb058ec
Timofey Nazarov <timothy96@rambler.ru>
2017-03-17 21:08:23 +0300

    (51) post request json logic implemented

--- cinema/account/views.py
+++ cinema/account/views.py
@@ -14,14 +14,16 @@ def auth_user(request, user_json='{"email": "test@test.com", "password": "123456
     else:
         return HttpResponse('not ok')
 
-def create_user(request, user_json='{"email": "test@test.com", "password": "12345678", "phone = "123"}'):
-    user_info = json.loads(user_json)
-    user_phone = int(user_info["phone"])
-    if user_phone is not None:
-        user = User.objects.create_user(email=user_info["email"], password=user_info["password"], phone=user_phone)
-        return HttpResponse("user "+user.email+" created")
-    else:
-        return HttpResponse("not ok")
+def create_user(request):
+    if request.method == 'POST':
+        user_info = json.loads(request.body.decode, encoding='UTF-8')
+        user_phone = int(user_info["phone"])
+        if user_phone is not None:
+            user = User.objects.create_user(email=user_info["email"], password=user_info["password"], phone=user_phone)
+            return HttpResponse("user "+user.email+" created")
+        else:
+            return HttpResponse("not ok")
 
 def account_info(request, id=0):
+    this_user = User.objects.get(id=id)
     return HttpResponse("account page dummy")

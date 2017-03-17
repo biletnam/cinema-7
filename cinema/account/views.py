c39193e9f1e4cfc6dd53c626f5883317e96e7d77
Timofey Nazarov <timothy96@rambler.ru>
2017-03-17 19:21:58 +0300

    (45) update for views.py

--- cinema/account/views.py
+++ cinema/account/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from django.shortcuts import render
+# from cinema.account.models import UserManager, User
+# # from django.contrib.auth import authenticate as auth
+# import json
+#
+# def auth_user(request, user_json):
+#     user_info = json.loads(user_json)
+#     print(user_info)
+#     return

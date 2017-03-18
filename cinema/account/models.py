c8b533ce1b8c811ab97f5432922e595006989ad5
Ilya <dontmarepad@gmail.com>
2017-03-18 03:03:35 +0300

    change to charfield

--- cinema/account/models.py
+++ cinema/account/models.py
@@ -26,7 +26,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
-    phone = models.IntegerField(_('phone'), default=0)
+    phone = models.CharField(_('phone'), max_length=30)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)

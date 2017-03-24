e1ad904bec062d3b89a378019a66b369ad9874f7
Timofey Nazarov <timothy96@rambler.ru>
2017-03-24 18:09:26 +0300

    booking reference fix

--- cinema/booking/models.py
+++ cinema/booking/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from cinema.schedule.models import Seance, Row, Hall, Seat, Seat_new
+from cinema.schedule.models import Seance, Row, Hall, Seat
 from django.contrib.postgres import fields
 from django.conf import settings
 from django.db.models.signals import pre_delete
@@ -14,8 +14,9 @@ class Booking(models.Model):
 
     def str_seats(self):
         return_string = ""
-        for seat in self.seats:
-            return_string += (str(seat) + ", ")
+        booked_seats = Seat.objects.filter(booking=self)
+        for seat in booked_seats:
+            return_string += (str(seat.row) + "_" + str(seat.number) + ", ")
         return(return_string)
 
     def __str__(self):
@@ -26,42 +27,9 @@ class Booking(models.Model):
 
     def get_movie(self):
         return self.seance.movie
-
 
 @receiver(pre_delete, sender=Booking)
-def booking_delete(sender, instance, **kwargs):
-    seance = instance.seance
-    hall = seance.hall
-    for seat in instance.seats:
-        indices = seat.split("_")
-        row = Row.objects.filter(hall=hall, number=int(indices[0]))
-        number = int(indices[1])
-        Seat.objects.filter(seance=seance, hall=hall, row=row, number=number).update(booked=False)
-
-
-class Booking_new(models.Model):
-    price = models.FloatField()
-    seance = models.ForeignKey(Seance)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
-
-    def str_seats(self):
-        return_string = ""
-        for seat in self.seats:
-            return_string += (str(seat) + ", ")
-        return(return_string)
-
-    def __str__(self):
-        return("Booking: " + str(self.id) + " " + self.seance.movie.title + " " + self.str_seats())
-
-    def get_id(self):
-        return self.id
-
-    def get_movie(self):
-        return self.seance.movie
-
-
-@receiver(pre_delete, sender=Booking_new)
 def booking_new_delete(sender, instance, **kwargs):
-    queryset = Seat_new.objects.filter(booking=sender)
+    queryset = Seat.objects.filter(booking=sender)
     for seat in queryset:
         seat.set_null_booking()

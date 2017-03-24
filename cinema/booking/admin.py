687a834cfe650758ce5c48ec6577db020e851bfd
Timofey Nazarov <timothy96@rambler.ru>
2017-03-24 17:50:37 +0300

    db changes to booikng app

--- cinema/booking/admin.py
+++ cinema/booking/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, Booking_new
 
-admin.site.register(Booking)
+admin.site.register(Booking, Booking_new)
--- cinema/booking/models.py
+++ cinema/booking/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from cinema.schedule.models import Seance, Row, Hall, Seat
+from cinema.schedule.models import Seance, Row, Hall, Seat, Seat_new
 from django.contrib.postgres import fields
 from django.conf import settings
 from django.db.models.signals import pre_delete
@@ -37,3 +37,31 @@ def booking_delete(sender, instance, **kwargs):
         row = Row.objects.filter(hall=hall, number=int(indices[0]))
         number = int(indices[1])
         Seat.objects.filter(seance=seance, hall=hall, row=row, number=number).update(booked=False)
+
+
+class Booking_new(models.Model):
+    price = models.FloatField()
+    seance = models.ForeignKey(Seance)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+
+    def str_seats(self):
+        return_string = ""
+        for seat in self.seats:
+            return_string += (str(seat) + ", ")
+        return(return_string)
+
+    def __str__(self):
+        return("Booking: " + str(self.id) + " " + self.seance.movie.title + " " + self.str_seats())
+
+    def get_id(self):
+        return self.id
+
+    def get_movie(self):
+        return self.seance.movie
+
+
+@receiver(pre_delete, sender=Booking_new)
+def booking_new_delete(sender, instance, **kwargs):
+    queryset = Seat_new.objects.filter(booking=sender)
+    for seat in queryset:
+        seat.set_null_booking()
